e93749cfa8a30fe6be562b4fbbc986406b536c9e
star__hoshi <kensuke1751@gmail.com>
2016-02-28 18:17:08 +0900

    create github-corner.d.ts.

---
+++ react-github-corner.d.ts
@@ -0,0 +1,20 @@
+/// <reference path="typings/main.d.ts" />
+
+declare module "react-github-corner" {
+  import * as React from 'react';
+
+  interface IGithubCornerProps extends React.Props<GithubCorner> {
+    href?: string;
+    width?: string | number;
+    height?: string | number;
+    direction?: string;
+    octoColor?: string;
+    bannerColor?: string;
+  }
+
+  interface GithubCorner extends React.ComponentClass<IGithubCornerProps> {
+  }
+  const GithubCornerr:GithubCorner;
+
+  export = GithubCorner;
+}
